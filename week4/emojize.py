ab23b4ccb1036c66138b3c8f419332f03dd8e50c
Nicole-Lynn <160227769+Nicole-Lynn@users.noreply.github.com>
2024-04-27 00:02:10 +0300

    Delete week4/emojize.py

--- week4/emojize.py
+++
@@ -1,8 +0,0 @@
-import emoji
-
-# Using emoji library to convert emojis from words to the actual emoji eg :thumbsup: - 👍
-in_put = input("Input: ")
-output = emoji.emojize(in_put, language = 'alias')
-
-print("Output:", output)
-
